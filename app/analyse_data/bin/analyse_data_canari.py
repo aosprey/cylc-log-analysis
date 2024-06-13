281cc607f00e52e55ba6b0cd18919e3b67ef449b
aosprey <4313200+aosprey@users.noreply.github.com>
2024-06-13 15:01:58 +0100

    Update plot titles; add methods to read and write text files.

--- app/analyse_data/bin/analyse_data_canari.py
+++ app/analyse_data/bin/analyse_data_canari.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from datetime import datetime
+import pandas as pd
 import numpy as np
 import os
 from cylc_performance import *
@@ -29,30 +29,31 @@ def coupled_data(data_dir='.', plot_dir='.'):
 
     # Plots 
     image_dir = plot_dir+'/IMAGES'
-    ens_label = 'CANARI LE on ARCHER2'
+    ens_label = 'CANARI LE on ARCHER2: '
+
     coupled.plot_queue_time(
         plot_file=image_dir+'/coupled_queue_time.png', 
-        title='Job queue times: '+ens_label,
-        suites=suites_3m,
-	mean=True,
-	hlines=np.arange(10,50,10))
+        title=ens_label+'Job queue times', 
+        suites=suites_3m, 
+        mean=True, 
+        hlines=np.arange(10,50,10))
     coupled.plot_runtime_filesystem(
         plot_file=image_dir+'/coupled_runtime.png', 
-        title='Run times per model month: '+ens_label,
+        title=ens_label+'Run times per model month',
         suites=suites_3m,
 #	hlines=np.arange(2,10,2),
         status=True, 
         xios_logs=True) 
     coupled.plot_sypd(
         plot_file=image_dir+'/coupled_SYPD.png', 
-        title='SYPD per model month: '+ens_label,
+        title=ens_label+'SYPD per model month',
         suites=suites_3m,
 	hlines=np.arange(0.8,2.6,0.4),
         y_ticks=np.arange(0.8,2.6,0.2),
         mean=True) 
     coupled.plot_daily_status(
         plot_file=image_dir+'/coupled_status.png', 
-        title='Model task statuses per day: '+ens_label,
+        title=ens_label+'Model task statuses per day',
         suites=suites_3m,
         mean=True, 
 	hlines=np.arange(20,110,20))
@@ -74,7 +75,7 @@ def coupled_data(data_dir='.', plot_dir='.'):
     timestamp_html('coupled_plots.html', data_dir, plot_dir) 
     total_sy_hist = suite_status.data['Completed years'].loc[suites_hist].sum()
     total_sy_ssp = suite_status.data['Completed years'].loc[suites_ssp].sum()
-    populate_index_html('index.html', data_dir, plot_dir, total_sy_hist, total_sy_ssp, perf_html_hist, perf_html_ssp)
+    populate_html('index.html', data_dir, plot_dir, total_sy_hist, total_sy_ssp, perf_html_hist, perf_html_ssp)
     
 def pptransfer_data(data_dir='.', plot_dir='.'): 
     """Generate performance plots for pptransfer jobs."""
@@ -87,65 +88,57 @@ def pptransfer_data(data_dir='.', plot_dir='.'):
 
     # Plots
     image_dir = plot_dir+'/IMAGES'
-    ens_label = 'CANARI LE on ARCHER2'
+    ens_label = 'CANARI LE on ARCHER2: '
     pptransfer.plot_daily_status(
         plot_file=image_dir+'/pptransfer_status.png', 
-        title='Transfer task statuses per day: '+ens_label, 
+        title=ens_label+'Transfer task statuses per day', 
 	mean=True, 
 	hlines=[50,100,150,200],
 	y_ticks=np.arange(5,30,5))
     pptransfer.plot_speed(
         plot_file=image_dir+'/pptransfer_speed.png', 
-	title='Transfer task speed: '+ens_label, 
+	title=ens_label+'Transfer task speed',
 	mean=True, 
         hlines=[50,100,150,200])
     timestamp_html('pptransfer_plots.html', data_dir, plot_dir) 
     
 def timestamp_html(template_file, data_dir, plot_dir): 
     """Add timesamp to html template file."""
-    f = open(template_file, 'r')
-    contents = f.read()
-    f.close()
-
-    now = datetime.now() 
+    contents = read_file(template_file) 
+    now = pd.Timestamp.now()
     now_str = now.strftime('%Y-%m-%d %H:%M:%S')
     contents = contents.replace('XX_DATE_XX', now_str) 
+    write_file(plot_dir+'/'+template_file, contents) 
     
+def populate_html(template_file, data_dir, plot_dir, 
+                  total_sy_hist, total_sy_ssp, perf_file_hist, perf_file_ssp): 
+    """
+    Generate html based on perf stats and total SY for HIST2 and SSP370 enembles. 
+    """
+    timestamp_html(template_file, data_dir, plot_dir)
     out_file = plot_dir+'/'+template_file
-    f = open(out_file, 'w') 
-    f.write(contents) 
-    f.close()
-    
-def populate_index_html(template_file, data_dir, plot_dir, 
-                        total_sy_hist, total_sy_ssp, perf_file_hist, perf_file_ssp): 
-    """
-    Generate index html based on perf stats and total SY for HIST2 and SSP370 enembles. 
-    """
-    timestamp_html(template_file, data_dir, plot_dir) 
-    
-    in_file = plot_dir+'/'+template_file
+    contents = read_file(out_file)     
+    contents = contents.replace('XX_SY_HIST_XX', str(round(total_sy_hist))) 
+    contents = contents.replace('XX_SY_SSP_XX', str(round(total_sy_ssp))) 
+    table = read_file(perf_file_hist)
+    contents = contents.replace('XX_TABLE_HIST_XX', table) 
+    table = read_file(perf_file_ssp)
+    contents = contents.replace('XX_TABLE_SSP_XX', table) 
+    write_file(out_file, contents)
+
+def read_file(in_file): 
+    """Read file and return contents"""
     f = open(in_file, 'r')
     contents = f.read()
     f.close()
-    
-    contents = contents.replace('XX_SY_HIST_XX', str(round(total_sy_hist))) 
-    contents = contents.replace('XX_SY_SSP_XX', str(round(total_sy_ssp))) 
+    return contents     
 
-    f = open(perf_file_hist, 'r')
-    table = f.read()
-    f.close()
-    contents = contents.replace('XX_TABLE_HIST_XX', table) 
-	
-    f = open(perf_file_ssp, 'r')
-    table = f.read()
-    f.close()
-    contents = contents.replace('XX_TABLE_SSP_XX', table) 
-    
-    out_file = in_file
+def write_file(out_file, contents): 
+    """Write contents to file."""   
     f = open(out_file, 'w') 
     f.write(contents) 
     f.close()
-     
+
 if __name__=='__main__': 
     data_dir = os.environ.get('DATA_DIR', '/gws/nopw/j04/canari/users/aosprey/log-analysis/data')
     plot_dir = os.environ.get('PLOT_DIR', './plots')
